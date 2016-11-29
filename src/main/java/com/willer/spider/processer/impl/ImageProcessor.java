21d5112c2b7bd4736122d092a0682599f51366f0
xianliangzhang <2497095564@qq.com>
2016-11-30 00:27:24 +0800

    Spider

--- src/main/java/com/willer/spider/processer/impl/ImageProcessor.java
+++ src/main/java/com/willer/spider/processer/impl/ImageProcessor.java
@@ -2,14 +2,14 @@ package com.willer.spider.processer.impl;
 
 import com.willer.common.ConfigHelper;
 import com.willer.spider.processer.Processor;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.digester.Digester;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.jsoup.nodes.Document;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -21,7 +21,7 @@ import java.util.UUID;
  */
 public class ImageProcessor implements Processor {
     private static final Logger RUN_LOG = Logger.getLogger(ImageProcessor.class);
-    private static final long DEFAULT_MIN_IMAGE_SIZE = 100*1024; // 默认最小下载128K的图片
+    private static final long DEFAULT_MIN_IMAGE_SIZE = 10*1024; // 默认最小下载128K的图片
     private static final long DEFAULT_MAX_IMAGE_SIZE = 1*1024*1024*1024; // 默认最大下载1G的图片
     private static final String DEFAULT_IMAGE_SUFFIX = "jpg,jpeg,png,gif";
 
@@ -30,8 +30,6 @@ public class ImageProcessor implements Processor {
             Long.valueOf(ConfigHelper.get("spider.min.image.size")) : DEFAULT_MIN_IMAGE_SIZE;
     private static final long MAX_IMAGE_SIZE = ConfigHelper.containsKey("spider.max.image.size") ?
             Long.valueOf(ConfigHelper.get("spider.max.image.size")) : DEFAULT_MAX_IMAGE_SIZE;
-    private static final Set<String> IMAGE_SUFFIX = new HashSet<String>(Arrays.asList( ConfigHelper.containsKey("spider.image.suffix") ?
-            ConfigHelper.get("spider.image.suffix") : DEFAULT_IMAGE_SUFFIX ));
 
     static {
         File tempImgDir = new File(STORE_IMG_DIR);
@@ -54,35 +52,50 @@ public class ImageProcessor implements Processor {
     private void download(String url) throws Exception {
         RUN_LOG.info(String.format("Start Process Image [url=%s]", url));
 
-        String fileName = url.substring(url.lastIndexOf("/")+1).trim();
-//        String imageSuffix = fileName.substring(fileName.lastIndexOf("."));
-//        if (StringUtils.isEmpty(imageSuffix)) {
-//            throw new RuntimeException(String.format("Image-Suffix Wrong [Target-Suffix=%s, Current-Suffix=%s]", IMAGE_SUFFIX, imageSuffix));
-//        }
-
-        File file = new File(STORE_IMG_DIR.concat("/").concat(fileName));
-        if (file.exists()) {
-            file = new File(STORE_IMG_DIR.concat("/").concat(UUID.randomUUID().toString()).concat(fileName));
+        String imageSuffix = url.substring(url.lastIndexOf(".") + 1);
+        if (StringUtils.isEmpty(imageSuffix) || !DEFAULT_IMAGE_SUFFIX.contains(imageSuffix)) {
+            throw new RuntimeException(String.format("Image-Suffix Wrong [Target-Suffix=%s, Current-Suffix=%s]", DEFAULT_IMAGE_SUFFIX, imageSuffix));
         }
 
-        InputStream inputStream = new URL(url.trim()).openStream();
-        OutputStream outputStream = new FileOutputStream(file);
+        File file = new File(STORE_IMG_DIR.concat("/").concat(UUID.randomUUID().toString()).concat(".").concat(imageSuffix));
+        File tempTargetFile = transfer(new URL(url).openStream(), file);
+        RUN_LOG.info(String.format("Image-Download [image=%s, size=%d]", file.getName(), tempTargetFile.length()));
+        if (tempTargetFile.length() > MAX_IMAGE_SIZE || tempTargetFile.length() < MIN_IMAGE_SIZE) {
+            file.deleteOnExit();
+            RUN_LOG.warn(String.format("Image-Size Wrong And Deleted [Target-Min-Size=%d, Target-Max-Size=%d, Current-Size=%d]",
+                    MIN_IMAGE_SIZE, MAX_IMAGE_SIZE, tempTargetFile.length()));
+        }
+
+        // 取文件md5
+        InputStream inputStream = new FileInputStream(tempTargetFile);
+        String md5hex = DigestUtils.md5Hex(inputStream);
+        IOUtils.closeQuietly(inputStream);
+
+        // 以md5重命名
+        File targetFile = new File(STORE_IMG_DIR.concat("/").concat(md5hex).concat(".").concat(imageSuffix));
+        if (targetFile.exists()) {
+            targetFile.deleteOnExit();
+            RUN_LOG.warn(String.format("Target File Exists [file=%s]", targetFile.getName()));
+        } else {
+            transfer(new FileInputStream(tempTargetFile), targetFile);
+            tempTargetFile.deleteOnExit();
+        }
+    }
+
+    private File transfer(InputStream inputStream, File targetFile) throws Exception {
+        if (!targetFile.getParentFile().exists()) {
+            targetFile.getParentFile().mkdirs();
+        }
+
+        OutputStream outputStream = new FileOutputStream(targetFile);
         byte[] buffer = new byte[4096];
         int readSize = -1;
-        int totalReadSize = 0;
         while ((readSize = inputStream.read(buffer)) != -1) {
             outputStream.write(buffer, 0, readSize);
-            totalReadSize += readSize;
         }
         IOUtils.closeQuietly(inputStream);
         IOUtils.closeQuietly(outputStream);
-
-        RUN_LOG.info(String.format("Image-Download [image=%s, size=%d]", fileName, totalReadSize));
-        if (totalReadSize > MAX_IMAGE_SIZE || totalReadSize < MIN_IMAGE_SIZE) {
-            file.deleteOnExit();
-            RUN_LOG.warn(String.format("Image-Size Wrong And Deleted [Target-Min-Size=%d, Target-Max-Size=%d, Current-Size=%d]",
-                    MIN_IMAGE_SIZE, MAX_IMAGE_SIZE, totalReadSize));
-        }
+        return targetFile;
     }
 
     public static void main(String[] args) throws Exception {
