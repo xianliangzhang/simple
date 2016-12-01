4536f9b2d96803b7d2f9fbaf759609ddceeeee6a
zhnagxianliang1 <zhangxianliang1@kingsoft.com>
2016-12-01 21:31:25 +0800

    Rebuild

--- src/main/java/sexy/kome/core/ds/KomeDataSource.java
+++ src/main/java/sexy/kome/core/ds/KomeDataSource.java
@@ -1,12 +1,29 @@
 package sexy.kome.core.ds;
 
 import com.willer.common.ConfigHelper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import sexy.kome.core.ds.operator.dao.OperatorMapper;
+import sexy.kome.core.ds.operator.model.Operator;
+
+import java.io.Reader;
+import java.util.Properties;
 
 /**
  * Created by Hack on 2016/12/1.
  */
 public class KomeDataSource {
-    public static void main(String[] args) {
-        System.out.println(ConfigHelper.get("KOME_X"));
+
+    public static void main(String[] args) throws Exception {
+        Properties properties = new Properties();
+        properties.setProperty("username", ConfigHelper.get("_env.KOME_X"));
+        properties.setProperty("password", ConfigHelper.get("_env.KOME_Y"));
+
+        Reader reader = Resources.getResourceAsReader("mybatis.xml");
+
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(reader, properties);
+        Operator operator = factory.openSession().getMapper(OperatorMapper.class).find(1L);
+        System.out.println(operator.getId());
     }
 }
--- src/main/java/sexy/kome/core/ds/operator/dao/OperatorMapper.java
+++ src/main/java/sexy/kome/core/ds/operator/dao/OperatorMapper.java
@@ -1,13 +1,11 @@
 package sexy.kome.core.ds.operator.dao;
 
-import org.apache.ibatis.annotations.Select;
 import sexy.kome.core.ds.operator.model.Operator;
 
 /**
  * Created by Hack on 2016/12/1.
  */
 public interface OperatorMapper {
-    @Select("SELECT * FROM operator WHERE id = #{id}")
-    public Operator find(Long id);
-    public void save(Operator operator);
+    Operator find(Long id);
+    void save(Operator operator);
 }
